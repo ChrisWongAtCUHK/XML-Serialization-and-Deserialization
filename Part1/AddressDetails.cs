6d5474263d515bf72a1b1d8ed6b611ba4c426336
Chris Wong <wcwong@MaximizerSoftware.onmicrosoft.com>
2017-01-26 08:28:13 +0800

    Introduction.

---
+++ Part1/AddressDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Part1
+{
+    public class AddressDetails
+    {
+        public int HouseNo { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        private string PoAddress { get; set; }
+    }
+}
---
+++ Part1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressDetails details = new AddressDetails();
+            details.HouseNo = 4;
+            details.StreetName = "Rohini";
+            details.City = "Delhi";
+            Serialize(details);
+        }
+
+        static public void Serialize(AddressDetails details)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(AddressDetails));
+            using (TextWriter writer = new StreamWriter(@"Xml.xml"))
+            {
+                serializer.Serialize(writer, details);
+            }
+        }
+    }
+}
